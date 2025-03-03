0a16c800b48e5fec361be0b625a49b41565a4e46
dinakaranak <dinakaranak001@gmail.com>
2025-03-03 17:32:10 +0530

    css

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react'
 import Weather from './Component/Weather'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Signup from './Component/Signup'
+import Contact from './Component/Contact'
 
 function App() {
   return (
     <div>
         <BrowserRouter>
       <Routes>
-        <Route path='/Weather' element={<Weather />}></Route>
+        <Route path='/' element={<Weather />}></Route>
         <Route path='/Signup' element={<Signup />}></Route>
+        <Route path='/Contact' element={<Contact />}></Route>
        
 
       </Routes>
--- src/Component/Weather.js
+++ src/Component/Weather.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
 import './Weather.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Container, Nav, Navbar, NavDropdown, Card } from 'react-bootstrap';
+import { Button, Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { Link, Navigate } from 'react-router-dom';
+import weath from '../Component/weath.webp'
+import degree from '../Component/degree.jpg'
+import unnamed from '../Component/unnamed.png'
+import Signup from './Signup';
+import Contact from './Contact';
 
 // import back from '../Component/img.webp'
 const Weather = () => {
@@ -46,20 +51,22 @@ const Weather = () => {
     };
 
     return (
+        <div>
         <div className='full'>
             <div>
             <Navbar expand="lg" className="bg-body-tertiary nav">
                 <Container>
-                    <Navbar.Brand href="#">API Card Functions</Navbar.Brand>
+                       <Navbar.Brand href="#">Weather</Navbar.Brand>
+                    <img src={weath} className='logo'></img>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#">Home</Nav.Link>
+                            <Nav.Link href="/Weather">Home</Nav.Link>
                             <Nav.Link href="#">Contact</Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#">Action</NavDropdown.Item>
                                 <NavDropdown.Item href="#">Another action</NavDropdown.Item>
-                             </NavDropdown> 
+                             </NavDropdown>  */}
                         </Nav>
                         <Link to='/Signup'>
                             <Button variant="outline-primary" className="ms-3 btn-outline-primary">Sign Up / Login</Button>
@@ -68,10 +75,9 @@ const Weather = () => {
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-
             </div>
         <div className="weather-container">
-            <h1>Weather App</h1>
+            <h1 className='text'>Weather App</h1>
             <input 
                 type="text" 
                 className="weather-input"
@@ -86,15 +92,23 @@ const Weather = () => {
             
             {weather && (
                 <div className="weather-info">
+
                     <h2>Weather in {weather.name}</h2>
                     <p>Temperature: {weather.main.temp}°C</p>
                     <p>Description: {weather.weather[0].description}</p>
                     <p>Humidity: {weather.main.humidity}%</p>
                     <p>Wind Speed: {weather.wind.speed} m/s</p>
+                    <img src={degree} className='degree'></img>
                 </div>
             )}
         </div>
         </div>
+        <div>
+           <Contact />
+          
+        </div>
+        </div>
+    
     );
 };
 
